ecce006967318bcca2510d6c59f08bb95b74c3ff
henick408 <marcel.snopkowski@gmail.com>
2025-11-17 20:19:52 +0100

    3_2.2

---
+++ datasets.js
@@ -0,0 +1,31 @@
+document.getElementById("loadBtn").addEventListener("click", loadDatasets);
+
+async function loadDatasets() {
+  try {
+    const response = await fetch("http://localhost:3000/datasets");
+
+    if (!response.ok) {
+      throw new Error("Błąd API NOAA: " + response.status);
+    }
+
+    const data = await response.json();
+    const tableBody = document.querySelector("#datasetsTable tbody");
+    tableBody.innerHTML = "";
+
+    data.results.forEach(dataset => {
+      const row = document.createElement("tr");
+
+      row.innerHTML = `
+        <td>${dataset.id}</td>
+        <td>${dataset.name || "-"}</td>
+        <td>${dataset.datacoverage || "-"}</td>
+      `;
+
+      tableBody.appendChild(row);
+    });
+
+  } catch (error) {
+    console.error("Błąd:", error);
+    alert("Nie udało się pobrać datasetów NOAA.");
+  }
+}
