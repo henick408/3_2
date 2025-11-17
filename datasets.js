35c53c6e2f7fed32283140258e6d3a35d68bc22f
henick408 <marcel.snopkowski@gmail.com>
2025-11-17 21:06:04 +0100

    3_2.2

--- datasets.js
+++ datasets.js
@@ -17,8 +17,10 @@ async function loadDatasets() {
 
       row.innerHTML = `
         <td>${dataset.id}</td>
-        <td>${dataset.name || "-"}</td>
-        <td>${dataset.datacoverage || "-"}</td>
+        <td>${dataset.name }</td>
+        <td>${dataset.datacoverage }</td>
+        <td>${dataset.mindate }</td>
+        <td>${dataset.maxdate }</td>
       `;
 
       tableBody.appendChild(row);
